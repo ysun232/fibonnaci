1906c1c1c170d45cbc5aa37a6b1c2928c75a3e78
SUN YILONG <YSUN232@HOTMAIL.COM>
2018-09-29 22:38:16 -0400

    adding this

--- fibonnaci.java
+++ fibonnaci.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class fibonnaci{
-    
+    //this will take forever to compile the second the number gets a tiny bit bigger such as 100
     public static int fibonnaci(int x){
         if(x == 0){
             return 0;
@@ -14,15 +14,15 @@ public class fibonnaci{
         }
     }
     
+    
     public static void main (String[] args){
+    	
         Scanner keyboard= new Scanner(System.in);
-        
         System.out.println("Please input the fibonnaci number that you wish to find");
-        
+        //calling onto the scanner for the input from the user
         int number= keyboard.nextInt();
-        
         int b = fibonnaci(number);
-        
+        //test out this code for integers 5, 35, 45, 100. Compiling time would vastly vary
         System.out.println(b);
         
         
